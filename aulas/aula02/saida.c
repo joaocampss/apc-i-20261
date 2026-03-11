d87a42e57e63e5bba72ce8eac36864c0fd7045b6
unknown <2612082018@ACADEMICO.IESB.BR>
2026-03-11 10:57:29 -0300

    adiciona condigos da aula02

---
+++ aulas/aula02/saida.c
@@ -0,0 +1,15 @@
+#include <stdio.h> // inclusao de bibliotecas da linguagem 
+
+int main() {
+   //1.entrada 
+   
+   //2.processamento
+
+   //3.saida
+   printf("um texto qualquer\n");
+   printf("progamar e legal\n");
+   printf ("%i\n" ,10);
+   printf("%f\n",10.3455);
+
+    return 0;
+}
