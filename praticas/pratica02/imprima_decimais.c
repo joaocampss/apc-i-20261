6bcc8ef39a541ef53553d02011f4954aefc3cda0
joao <joaocampante19@gmail.com>
2026-04-08 10:05:30 -0300

    feat: conclui pratica02. Fecha #ID

---
+++ praticas/pratica02/imprima_decimais.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main () {
+  printf("%f\n", 3.1415926);
+  printf("%.2f\n", 3.14);
+
+ return 0;   
+}
---
+++ praticas/pratica02/imprima_inteiros.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main () {
+  int numero;
+  numero = 00004;
+  printf("%i\n", 10);
+  printf("%i\n", -5);
+  printf("00000%i\n", numero);
+
+ return 0;   
+}
