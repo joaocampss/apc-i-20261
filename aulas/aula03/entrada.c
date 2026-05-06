0a3d6da1e4c09539690c3c08d6f80448a25f252f
joao <joaocampante19@gmail.com>
2026-05-06 09:26:50 -0300

    aula05

---
+++ aulas/aula03/entrada.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main() {
+  
+  char tecla_pressionada;
+
+  printf("precione uma tecla\n");
+  scanf("%c", &tecla_pressionada);
+  printf( "voce precionou a tecla %c\n", tecla_pressionada);
+  while (getchar( )!= '\n')
+  int idade;
+
+  
+
+  //2.processamento
+
+
+  //3.saida
+
+
+
+}
